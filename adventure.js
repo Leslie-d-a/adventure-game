a93723a53af575054fe733fd30343e2388b79614
leslie <lesloo@protonmail.com>
2020-12-14 11:03:42 +0100

    game engine done

---
+++ adventure.js
@@ -0,0 +1,24 @@
+var currentLevel = 0;
+const TITLE = document.getElementById("levelTitle");
+const DESCRIPTION = document.getElementById("levelDescription");
+const BUTTONSDIV = document.getElementById("buttons");
+
+function levelRefresh(){
+    TITLE.innerHTML = levels[currentLevel].title;
+    DESCRIPTION.innerHTML = levels[currentLevel].description;
+    while(BUTTONSDIV.lastChild){
+        BUTTONSDIV.lastChild.remove();
+    }
+    for (i=0;i<levels[currentLevel].buttons;i++){
+        var button = document.createElement('button');
+        button.innerHTML = levels[currentLevel].buttonInfo[i].text;
+        button.setAttribute('number', i);
+        button.addEventListener('click', function(e){
+            currentLevel = levels[currentLevel].buttonInfo[e.target.getAttribute('number')].toLevel;
+            levelRefresh();
+        })
+        BUTTONSDIV.appendChild(button);
+    }
+}
+
+levelRefresh();
---
+++ levels.js
@@ -0,0 +1,28 @@
+var levels = [
+    {
+        "title":"Main Menu",
+        "description":"Begin van het spel",
+        "buttons":1,
+        "buttonInfo":[
+            {
+                "text":"To first level",
+                "toLevel":1
+            }
+        ]
+    },
+    {
+        "title":"Level 1",
+        "description":"Level 1 van game",
+        "buttons":2,
+        "buttonInfo":[
+            {
+                "text":"to second level",
+                "toLevel":2
+            },
+            {
+                "text":"death 1",
+                "toLevel":0
+            }
+        ]
+    }
+]
